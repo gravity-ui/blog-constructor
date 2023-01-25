490823ceb198f15a42450d7b172e6c20d4f0a2a5
ngorin <ngorin@yandex-team.ru>
2023-01-25 17:13:15 +0300

    feat: add workflows, update @types/react, small refactors

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -26,7 +26,7 @@ export default create({
     inputTextColor: 'black',
     inputBorderRadius: 4,
 
-    brandUrl: 'https://bb.yandex-team.ru/projects/DATA-UI/repos/blog-constructor/browse',
+    brandUrl: 'https://github.com/gravity-ui/blog-constructor',
     brandTitle: `<div style="font-size: 18px; color: #027bf3; font-weight: 600; margin-top: -6px; margin-bottom: 2px;">Blog Constructor</div>
-                <div style="font-size: 14px;color: #7d7d7d;font-weight: 400;">Data UI Guidelines</div>`,
+                <div style="font-size: 14px;color: #7d7d7d;font-weight: 400;">Gravity UI Guidelines</div>`,
 });
