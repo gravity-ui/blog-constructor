8f913116a8b268b89feb0457d8a6ad6fa4ff1500
aeksandla <aeksandla@yandex-team.ru>
2023-02-14 16:10:08 +0300

    feat: change source field with author description

--- src/blocks/Author/Author.tsx
+++ src/blocks/Author/Author.tsx
@@ -32,7 +32,7 @@ export const Author: React.FC<AuthorProps> = (props) => {
             description: author?.shortDescription || '',
             avatar: authorAvatar,
         };
-    }, [author?.avatar, author?.firstName, author?.fullDescription, author?.secondName, image]);
+    }, [author?.avatar, author?.firstName, author?.shortDescription, author?.secondName, image]);
 
     if (!authorItem?.firstName || !authorItem?.secondName) {
         return null;
