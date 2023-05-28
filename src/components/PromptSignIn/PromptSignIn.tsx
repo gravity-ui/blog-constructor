a903cdafc525e69d59ae6dcfd6ffad9ca9708114
Chris Co <coswarm@gmail.com>
2023-05-28 16:42:29 +0300

    minor css fix + add prop

--- src/components/PromptSignIn/PromptSignIn.tsx
+++ src/components/PromptSignIn/PromptSignIn.tsx
@@ -3,17 +3,20 @@ import React from 'react';
 import {Keyset, i18} from '../../i18n';
 import {Prompt, PromptProps} from '../Prompt/Prompt';
 
-export interface PromptSignInProps extends Partial<PromptProps> {}
+export interface PromptSignInProps extends Partial<PromptProps> {
+    onClickSignIn?: React.MouseEventHandler;
+}
 
 /**
  * Authentication Popup that appears when user action requires login
  */
 export function PromptSignIn({
     text = i18(Keyset.PromptSignInOnLike),
+    onClickSignIn = () => alert(i18(Keyset.SignIn)),
     actions = [
         {
             children: i18(Keyset.SignIn),
-            onClick: () => alert(i18(Keyset.SignIn)),
+            onClick: onClickSignIn,
         },
     ],
     ...props
