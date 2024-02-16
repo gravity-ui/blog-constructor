03eaa14e9b18395ee09f87740d3e2de2fb998b69
aeksandla <aeksandla@yandex-team.ru>
2024-02-16 15:16:42 +0300

    fix: fix a11y

--- src/demo/mocks.ts
+++ src/demo/mocks.ts
@@ -6,6 +6,7 @@ export const routerData = {
     hostname: 'host',
     query: {},
     updateQueryCallback: (params: Query) => {
+        // eslint-disable-next-line no-console
         console.log('params', params);
     },
 };
