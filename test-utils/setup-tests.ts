75719b9b1d794fb326898e9c3501af6edc50d14a
Nik Tverd <nn.tverd@gmail.com>
2024-03-05 00:09:53 +0500

    fixup! fixup! fix: test context added

--- test-utils/setup-tests.ts
+++ test-utils/setup-tests.ts
@@ -1,6 +1,5 @@
 import {Lang, configure as uiKitConfigure} from '@gravity-ui/uikit';
 import {configure} from '@testing-library/dom';
-import {Lang, configure as uiKitConfigure} from '@gravity-ui/uikit';
 
 uiKitConfigure({
     lang: Lang.En,
