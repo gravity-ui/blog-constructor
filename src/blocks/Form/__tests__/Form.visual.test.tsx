8a1d09212199bb2e623f1b351bb144dd0184c1ce
Nik Tverd <nn.tverd@gmail.com>
2024-09-26 20:09:11 +0500

    fixup! fixup! test: ci added

--- src/blocks/Form/__tests__/Form.visual.test.tsx
+++ src/blocks/Form/__tests__/Form.visual.test.tsx
@@ -7,13 +7,14 @@ import {Default, FormData} from './helpers';
 const FORM_DEALAY = 15 * 1000;
 
 test.describe('Form', () => {
-    test('render stories <Default>', async ({mount, expectScreenshot, delay}) => {
+    // skip because yandex forms falls shows Captcha
+    test.skip('render stories <Default>', async ({mount, expectScreenshot, delay}) => {
         await mount(<Default />);
         await delay(FORM_DEALAY);
         await expectScreenshot({skipTheme: 'dark'});
     });
 
-    test('render stories <FormData>', async ({mount, expectScreenshot, delay}) => {
+    test.skip('render stories <FormData>', async ({mount, expectScreenshot, delay}) => {
         await mount(<FormData />);
         await delay(FORM_DEALAY);
         await expectScreenshot({skipTheme: 'dark'});
