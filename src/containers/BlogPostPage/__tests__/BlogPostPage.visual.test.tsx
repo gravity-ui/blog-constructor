8d49f3adabe2e1c1ab0109f1806a8fec590e6e0b
aeksandla <aeksandla@yandex-team.ru>
2025-02-05 17:51:33 +0300

    feat: add mask for youtube

--- src/containers/BlogPostPage/__tests__/BlogPostPage.visual.test.tsx
+++ src/containers/BlogPostPage/__tests__/BlogPostPage.visual.test.tsx
@@ -7,15 +7,15 @@ import {Default, WithNavigation} from './helpers';
 const BLOG_POST_PAGE_DALAY = 10 * 1000;
 
 test.describe('BlogPostPage', () => {
-    test('render stories <Default>', async ({mount, expectScreenshot, delay}) => {
+    test('render stories <Default>', async ({mount, expectScreenshot, delay, page}) => {
         await mount(<Default />);
         await delay(BLOG_POST_PAGE_DALAY);
-        await expectScreenshot({skipTheme: 'dark'});
+        await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-Media__youtube')]});
     });
 
-    test('render stories <WithNavigation>', async ({mount, expectScreenshot, delay}) => {
+    test('render stories <WithNavigation>', async ({mount, expectScreenshot, delay, page}) => {
         await mount(<WithNavigation />);
         await delay(BLOG_POST_PAGE_DALAY);
-        await expectScreenshot({skipTheme: 'dark'});
+        await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-Media__youtube')]});
     });
 });
