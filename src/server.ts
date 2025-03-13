ffd9753e37baa8a2af49b81569db7566c288ba34
Aleksandra Pleshcheva <46083995+aeksandla@users.noreply.github.com>
2025-03-13 12:08:24 +0300

    feat: add export BlockType from server (#252)

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,4 @@ export {transformPageContent} from './data/transformPageContent';
 export {createReadableContent} from './data/createReadableContent';
 export {sanitizeMeta} from './data/sanitizeMeta';
 export {transformPost, TransformPostType} from './data/transformPost';
+export {BlockType} from './models/common';
