707ccb9be712ae21cbbe54a2d639255ea6c388ef
Aleksandra Pleshcheva <46083995+aeksandla@users.noreply.github.com>
2025-08-05 15:54:19 +0300

    feat(Header): add image out grid (#274)

--- src/blocks/Header/Header.tsx
+++ src/blocks/Header/Header.tsx
@@ -11,11 +11,16 @@ import {SettingsContext} from '../../contexts/SettingsContext';
 import {AnalyticsCounter} from '../../counters/utils';
 import {HeaderProps} from '../../models/blocks';
 import {PaddingsDirections} from '../../models/paddings';
+import {block} from '../../utils/cn';
 import {
     getBreadcrumbs,
     getBlogPath as getDefaultBlogPath,
     prepareAnalyticsEvent,
 } from '../../utils/common';
+
+import './Header.scss';
+
+const b = block('header-block');
 
 const analyticsEventsContainer: Record<string, AnalyticsEventsProp> = {
     sharing: prepareAnalyticsEvent({name: DefaultGoalIds.shareTop}),
@@ -28,7 +33,7 @@ const breadcrumbsGoals = prepareAnalyticsEvent({
 });
 
 export const Header = (props: HeaderProps) => {
-    const {theme, paddingTop, paddingBottom} = props;
+    const {theme, paddingTop, paddingBottom, imageInGrid = true} = props;
     const {post, breadcrumbs: customBreadcrumbs = {}} = React.useContext(PostPageContext);
     const {locale} = React.useContext(LocaleContext);
     const {getBlogPath = getDefaultBlogPath} = React.useContext(SettingsContext);
@@ -51,21 +56,27 @@ export const Header = (props: HeaderProps) => {
                 [PaddingsDirections.bottom]: paddingBottom,
             }}
         >
-            <HeaderBlock
-                {...props}
-                title={title}
-                description={description}
-                breadcrumbs={{...breadcrumbs, ...customBreadcrumbs}}
-            >
-                <PostInfo
-                    postId={id}
-                    date={date}
-                    readingTime={readingTime}
-                    analyticsEventsContainer={analyticsEventsContainer}
-                    theme={theme}
-                    qa="blog-header-meta-container"
-                />
-            </HeaderBlock>
+            <div className={b({'image-out-grid': !imageInGrid})}>
+                <HeaderBlock
+                    {...props}
+                    title={title}
+                    description={description}
+                    breadcrumbs={{...breadcrumbs, ...customBreadcrumbs}}
+                    mediaClassName={b('image')}
+                    gridClassName={b('grid')}
+                    contentWrapperClassName={b('content-wrapper')}
+                    contentInnerClassName={b('content-inner')}
+                >
+                    <PostInfo
+                        postId={id}
+                        date={date}
+                        readingTime={readingTime}
+                        analyticsEventsContainer={analyticsEventsContainer}
+                        theme={theme}
+                        qa="blog-header-meta-container"
+                    />
+                </HeaderBlock>
+            </div>
         </Wrapper>
     );
 };
