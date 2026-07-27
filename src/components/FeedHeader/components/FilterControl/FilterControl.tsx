bea899fb2fb37d00958385ad890a6a79812954e4
Aleksandra Pleshcheva <46083995+aeksandla@users.noreply.github.com>
2026-07-27 15:43:55 +0300

    fix(FilterControl): fix params in goals (#325)

--- src/components/FeedHeader/components/FilterControl/FilterControl.tsx
+++ src/components/FeedHeader/components/FilterControl/FilterControl.tsx
@@ -34,6 +34,13 @@ const addAnalyticsEventPostfix = (
         : addPostfix(analyticsEvents);
 };
 
+const getSelectedOptionNames = (selectedValues: string[], options: SelectFilterConfig['options']) =>
+    selectedValues.map((selectedValue) => {
+        const selectedOption = options.find((option) => option.value === selectedValue);
+
+        return selectedOption?.content || selectedValue;
+    });
+
 export type FilterControlProps = {
     filter: FilterConfig;
     initialValue: string | number | null | undefined;
@@ -113,7 +120,10 @@ export const FilterControl = ({filter, initialValue, onChange}: FilterControlPro
                 onOpen={() => handleFilterAnalytics('CLICK')}
                 onClose={({selectedValues, changesCount}) =>
                     handleFilterAnalytics('CLOSE', {
-                        selected_values: selectedValues.length ? selectedValues : null,
+                        selected_values: selectedValues.length
+                            ? getSelectedOptionNames(selectedValues, options)
+                            : null,
+                        selected_ids: selectedValues.length ? selectedValues : null,
                         changes_count: changesCount,
                         count_filters: selectedValues.length,
                     })
