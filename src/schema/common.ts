dd98d9f3ae7102e4b6850b8598d7a4b0a0d3d644
PahaN47 <pahan47@yandex-team.ru>
2026-08-25 17:10:39 +0300

    fix(Take): schema

--- src/schema/common.ts
+++ src/schema/common.ts
@@ -15,15 +15,14 @@ export const filteredItem = (itemsSchema: ObjectSchema) => ({
     },
 });
 
+export const PaddingSize = {
+    type: 'string',
+    enum: sizeTypes,
+};
+
 export const BlogBlockBase = {
-    paddingTop: {
-        type: 'string',
-        enum: sizeTypes,
-    },
-    paddingBottom: {
-        type: 'string',
-        enum: sizeTypes,
-    },
+    paddingTop: PaddingSize,
+    paddingBottom: PaddingSize,
     fullWidth: {
         type: 'boolean',
     },
